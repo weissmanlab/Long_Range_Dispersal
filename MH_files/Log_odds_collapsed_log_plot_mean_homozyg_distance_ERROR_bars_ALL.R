eeecb92d55cf296f52be8dea430f1aa77692e204
Tyler Smith <tylersmith@Tylers-MBP.attlocal.net>
2019-02-27 21:05:51 -0500

    improved plots and added zero dist data

--- MH_files/Log_odds_collapsed_log_plot_mean_homozyg_distance_ERROR_bars_ALL.R
+++ MH_files/Log_odds_collapsed_log_plot_mean_homozyg_distance_ERROR_bars_ALL.R
@@ -3,12 +3,13 @@ library(gridExtra)
 library(Hmisc)
 library(dplyr)
 
-par(mfrow=c(4,3))
+
+par(mfrow=c(2,2))
 x <- list()
 y <- list()
-p <- list()
+p <- list(4)
 k <- 1
-for(j in 1:4){#for(i in 0:2){
+for(j in 4:4){#for(i in 0:2){
 
 if(j==1)
 {alpha <- 1.25}
@@ -29,7 +30,7 @@ if(j==4)
 #{mu <- .001}
 
 
-1
+
 Simulation_Data <-   read.table("Total_outfile_ALL.txt")
 Semianalytic_Data <- read.table("formatted_semianalytic_data_ALL.txt")
 
@@ -50,11 +51,15 @@ Semianalytic_Data  <- subset(Semianalytic_Data, V1 == alpha)
 Simulation_Data <- Simulation_Data[!(Simulation_Data$V3 == 1 ),]
 Semianalytic_Data  <-Semianalytic_Data [!(Semianalytic_Data$V3 == 1 ),]
 
+if (alpha > 1.45)
+{#Simulation_Data <- Simulation_Data[!(Simulation_Data$V7 - Simulation_Data$V6  > 5),]
+#Semianalytic_Data <- Semianalytic_Data[!(Simulation_Data$V7 - Simulation_Data$V6  > 5),]
 
-Simulation_Data <- Simulation_Data[!(Simulation_Data$V7 - Simulation_Data$V6  > 5),]
-Semianalytic_Data <- Semianalytic_Data[!(Semianalytic_Data$V7 - Semianalytic_Data$V6  > 5),]
-
-Simulation_Data <- Simulation_Data[!(Simulation_Data$V5   < -30),]
+Simulation_Data <- Simulation_Data[!(Simulation_Data$V4 > 10),]
+Semianalytic_Data <- Semianalytic_Data[!(Semianalytic_Data$V4 > 10),]
+# last distance point has extremely large error bars.  Remove this simulation data w/ insufficient trials.
+}
+#Simulation_Data <- Simulation_Data[!(Simulation_Data$V5   < -30),]
 
 
 
@@ -76,6 +81,11 @@ Semianalytic_Data[4] <- Semianalytic_Data[4] + (1/Semianalytic_Data[1])*log(Semi
 Simulation_Data[5] <- Simulation_Data[5] - (1/Simulation_Data[1] - 1)*log(Simulation_Data[3])
 Semianalytic_Data[5] <- Semianalytic_Data[5] - (1/Semianalytic_Data[1] - 1)*log(Semianalytic_Data[3])
 
+Simulation_Data[6] <- Simulation_Data[6] - (1/Simulation_Data[1] - 1)*log(Simulation_Data[3])
+Semianalytic_Data[6] <- Semianalytic_Data[6] - (1/Semianalytic_Data[1] - 1)*log(Semianalytic_Data[3])
+
+Simulation_Data[7] <- Simulation_Data[7] - (1/Simulation_Data[1] - 1)*log(Simulation_Data[3])
+Semianalytic_Data[7] <- Semianalytic_Data[7] - (1/Semianalytic_Data[1] - 1)*log(Semianalytic_Data[3])
 #replace mean homozygosity/probability of identity with log of odds of identiy + log(rho)
 
 
@@ -91,14 +101,37 @@ colnames(Semianalytic_Data)[4] <- "log_of_distance_rescaled"
 colnames(Simulation_Data)[5] <- "log_odds_of_identity_rescaled"
 colnames(Semianalytic_Data)[5] <- "log_odds_of_identity_rescaled"
 
-p[[k]] <- ggplot() + geom_point(data=Semianalytic_Data, aes(log_of_distance_rescaled, log_odds_of_identity_rescaled), color = "red") + 
-        geom_pointrange(data=Simulation_Data, aes(x = log_of_distance_rescaled, y =log_odds_of_identity_rescaled, ymin =V6, ymax =V7), color="blue", pch = 0)  + ggtitle(paste("alpha", alpha))
-#print(p[[k]])
-k <- k + 1
+Rho <- as.factor(1/Simulation_Data[, 2])
+Mu <- as.factor(Simulation_Data[, 3])
+
+#colnames(Simulation_Data["V2"]) = "Rho_Inverse"
+#colnames(Simulation_Data["V3"]) = "Mu"
+#Simulation_Data["COLOR"] <- as.factor(-(log10(Simulation_Data[,3]) -14))
+#Simulation_Data["SHAPE"] <- as.factor(Simulation_Data[,2]) #as.factor(-log10(Simulation_Data[,2]) +1)
+
+
+p[[j]] <- ggplot(data=Simulation_Data, aes(x = log_of_distance_rescaled, y =log_odds_of_identity_rescaled, ymin =V6, ymax =V7, colour = Mu, shape = Rho)) + geom_point() +geom_pointrange() + ggtitle(paste("alpha", alpha)) + geom_smooth(data=Semianalytic_Data, colour = "brown") 
+
+
+#p[[k]]
+
+plot(p[[j]])
+
+
+#k <- k + 1
+
+#basic <- ggplot(mtcars, aes(wt, mpg, colour = factor(cyl), shape = factor(vs) )) + geom_point()
+
+
 
 #}
 
+
 }
-do.call(grid.arrange,p)
+
+#multiplot(p)
+#do.call(grid.arrange,p)
 
 ####################
+
+
