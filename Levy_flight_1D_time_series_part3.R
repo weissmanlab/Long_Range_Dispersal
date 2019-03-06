6be77e45bd2926f6ea53a57452824e134fdab66b
Tyler Smith <tylersmith@Tylers-MBP.attlocal.net>
2019-03-05 19:07:34 -0500

    making progress on part 3 - smoothing histogram

---
+++ Levy_flight_1D_time_series_part3.R
@@ -0,0 +1,61 @@
+library(stabledist)
+ALPHA = 1.25
+rho_inverse = 1
+distance = 0
+scale_parameter = 250
+
+
+
+
+#string = paste("./rho_inverse_", rho_inverse, "alpha_", alpha, sep = "")
+string = paste("./alpha_value_", ALPHA, "/distance_value_", distance, sep = "")
+#print(string)
+setwd(string)
+
+
+#print(Sys.glob(paste("histogram_of_boot*", rho_inverse, "*", sep = "")))
+
+for(k in 1:3)
+{
+
+   if(k == 1) {mu = 0.001}
+   if(k == 2) {mu = 0.01}
+   if(k == 3) {mu = 0.1}
+   
+
+
+if(rho_inverse != 1)
+{
+Bootstrapped_Mean_Homozygosities_Histogram <-  read.table(Sys.glob(paste("histogram_of_boot*", rho_inverse, "*", sep = "")))
+Single_Trial_Homozygosities_Histogram <-   read.table(Sys.glob(paste("histogram_of_single*", rho_inverse, "*", sep = "")))
+}
+if(rho_inverse == 1)
+{
+Bootstrapped_Mean_Homozygosities_Histogram <-  read.table(Sys.glob(paste("histogram_of_boot*", "*rho_inverse_1.txt", sep = "")))
+Single_Trial_Homozygosities_Histogram <-   read.table(Sys.glob(paste("histogram_of_single*",  "*rho_inverse_1.txt", sep = "")))
+}
+
+
+	Bootstrapped_Mean_Homozygosities_Histogram <- subset(Bootstrapped_Mean_Homozygosities_Histogram, V2 == mu)
+	
+	Single_Trial_Homozygosities_Histogram  <- subset(Single_Trial_Homozygosities_Histogram , V2 == mu)
+ #print(sum(Single_Trial_Homozygosities_Histogram[,4]))
+ #print(sum(Bootstrapped_Mean_Homozygosities_Histogram[,4]))
+
+
+dummy <- exp(-mu*1)
+
+dummy_vector <- dstable(x = distance, alpha = ALPHA , beta = 0, gamma = scale_parameter, delta = 0, pm = 0,log = FALSE,tol = 64*.Machine$double.eps, zeta.tol = NULL,subdivisions = 1000)
+
+
+
+#dummy2 <- dstable(x, alpha, beta, gamma)
+
+
+}
+
+
+
+
+
+setwd("./../../")
