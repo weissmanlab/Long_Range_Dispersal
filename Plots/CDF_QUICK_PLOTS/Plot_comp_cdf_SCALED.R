82e530eb1322e5112e477f7c774a6a30349320dd
Tyler Smith <tylersmith@Tylers-MacBook-Pro.local>
2019-10-15 13:34:19 -0400

    starting to plot scaled coalescence times

--- Plots/CDF_QUICK_PLOTS/Plot_comp_cdf_SCALED.R
+++ Plots/CDF_QUICK_PLOTS/Plot_comp_cdf_SCALED.R
@@ -10,15 +10,15 @@ library(ggpmisc)
 #CDF_table <-data.frame(read.table("CDF_dist0_c1.txt"))
 ##CDF_table <-data.frame(read.table("million_step_high_alpha_c_10_CDF.txt"))
 #CDF_table <-data.frame(read.table("million_step_high_alpha_c_5_CDF.txt"))
-CDF_table <-data.frame(read.table("million_step_alpha_1_c_1_CDF.txt"))
+#CDF_table <-data.frame(read.table("million_step_alpha_1_c_1_CDF.txt"))
 #CDF_table <-data.frame(read.table("10_6_runs_alpha_p75_CDF_of_coalescence_timesalpha_value_0.75distance_value_0000000rho_inverse_1e-05.txt"))
-#CDF_table <-data.frame(read.table("CDF_of_coalescence_timesalpha_value_0.5distance_value_0000000rho_inverse_1e-05.txt"))
+CDF_table <-data.frame(read.table("CDF_of_coalescence_timesalpha_value_0.5distance_value_0000000rho_inverse_1e-05.txt"))
 #CDF_table <-data.frame(read.table("CDF_of_coalescence_timesalpha_value_1distance_value_0000000rho_inverse_1000.txt"))
 scale_parameter <- 1
-alpha <- 1
+alpha <- .5
 rho <- 1
 #rho <- .001
-#rho <- 100000
+rho <- 100000
 rho_inverse <- 1/rho
 CDF_table <- subset(CDF_table, V1 == alpha)
 if(rho ==1){CDF_table <- subset(CDF_table, V2 == rho_inverse )}
@@ -42,15 +42,28 @@ print(improper_norm)
 
 if(alpha < 1)
 {
-CDF_table[,3] <- log10(CDF_table[,3] )
-CDF_table[,5] <- log10(improper_norm - CDF_table[,5] )
-CDF_table[,6] <- log10(improper_norm - CDF_table[,6] )
-CDF_table[,7] <- log10(improper_norm - CDF_table[,7] )
+CDF_table[,3] <- CDF_table[,3]*(2*(rho^alpha)*D)^(1/(1 - alpha))
 
-fun.1 <- function(x) (1- 1/alpha)*x + log10(gamma(1+1/alpha)/(((2*D)^(1/alpha))*pi*rho))  + log10(1/(1/alpha - 1))  
+
+CDF_table[,5] <- improper_norm - CDF_table[,5] 
+CDF_table[,6] <- improper_norm - CDF_table[,6] 
+CDF_table[,7] <- improper_norm - CDF_table[,7] 
+
+
+CDF_table[,5] <- CDF_table[,5]*pi*(1/alpha -1)/gamma(1/alpha +1)
+CDF_table[,6] <- CDF_table[,6]*pi*(1/alpha -1)/gamma(1/alpha +1)
+CDF_table[,7] <- CDF_table[,7]*pi*(1/alpha -1)/gamma(1/alpha +1)
+
+
+
+CDF_table[,3] <- log10(CDF_table[,3] )
+CDF_table[,5] <- log10(CDF_table[,5] )
+CDF_table[,6] <- log10(CDF_table[,6] )
+CDF_table[,7] <- log10(CDF_table[,7] )
+fun.1 <- function(x) (1- 1/alpha)*x  
 p <- ggplot(CDF_table, aes(V3, V5)) +
   geom_point() +  geom_pointrange(data=CDF_table, aes(x = V3, y =V5, ymin =V7, ymax =V6), pch  = 0) +
-stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0")) #+ xlim(0, 1.6)
+stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0")) 
   
   print(p)
 }
@@ -60,17 +73,32 @@ stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(pa
 
 if(alpha >1)
 {
-CDF_table[,3] <- log10(CDF_table[,3] )
-CDF_table[,5] <- log10(1 - CDF_table[,5] )
-CDF_table[,6] <- log10(1 - CDF_table[,6] )
-CDF_table[,7] <- log10(1 - CDF_table[,7] )
 
-if( alpha < 2){fun.1 <- function(x)   (1/alpha -1)*x + log10((alpha)*rho*((2*D)^(1/alpha))*sin(pi/alpha))} #log((alpha -1)*rho*(2*D)^(1/alpha)*sin(pi/alpha)) + log(1/(1-1/alpha))  
-if( alpha >= 2){fun.1 <- function(x)   (1/alpha -1)*x + log10((alpha)*rho*((2*D)^(1/alpha))*sin(pi/alpha))}
+CDF_table[,3] <- CDF_table[,3]*(2*(rho^alpha)*D)^(1/(1 - alpha))
+
+
+CDF_table[,5] <- 1 - CDF_table[,5] 
+CDF_table[,6] <- 1 - CDF_table[,6] 
+CDF_table[,7] <- 1 - CDF_table[,7] 
+
+
+
+CDF_table[,5] <- CDF_table[,5]/(alpha*sin(pi/alpha))
+CDF_table[,6] <- CDF_table[,6]/(alpha*sin(pi/alpha))
+CDF_table[,7] <- CDF_table[,7]/(alpha*sin(pi/alpha))
+
+
+
+CDF_table[,3] <- log10(CDF_table[,3] )
+CDF_table[,5] <- log10(CDF_table[,5] )
+CDF_table[,6] <- log10(CDF_table[,6] )
+CDF_table[,7] <- log10(CDF_table[,7] )
+
+fun.1 <- function(x)   ( 1/alpha - 1)*x 
 
 p <- ggplot(CDF_table, aes(V3, V5)) +
   geom_point() +  geom_pointrange(data=CDF_table, aes(x = V3, y =V5, ymin =V7, ymax =V6), pch  = 0) +
-stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0"))
+stat_function(fun=fun.1) + labs( x = "Log Scaled Time", y ="Log Scaled Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0"))
 
  
   
