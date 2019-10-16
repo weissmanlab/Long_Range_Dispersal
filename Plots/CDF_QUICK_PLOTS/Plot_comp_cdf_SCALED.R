8fdddd39c97683881fdacf475f4ee2af7a6d307d
Tyler Smith <tylersmith@Tylers-MBP.attlocal.net>
2019-10-16 16:00:29 -0400

    Numerics and analytics match.  Next make scaled plots.

--- Plots/CDF_QUICK_PLOTS/Plot_comp_cdf_SCALED.R
+++ Plots/CDF_QUICK_PLOTS/Plot_comp_cdf_SCALED.R
@@ -16,13 +16,14 @@ library(ggpmisc)
 #CDF_table <-data.frame(read.table("CDF_of_coalescence_timesalpha_value_0.75distance_value_0000000rho_inverse_1e-05.txt"))
 #CDF_table <-data.frame(read.table("CDF_of_coalescence_timesalpha_value_1distance_value_0000000rho_inverse_1000.txt"))
 #CDF_table <-data.frame(read.table("CDF_runs_alpha_1_dist0_c_250.txt"))
-CDF_table <-data.frame(read.table("CDF_runs_high_alpha_c_5.txt"))
+#CDF_table <-data.frame(read.table("CDF_runs_high_alpha_c_5.txt"))
+CDF_table <-data.frame(read.table("CDF_runs_alpha_1_c_1.txt"))
 #CDF_table <-data.frame(read.table("CDF_runs_low_alpha_c1.txt"))
-scale_parameter <- 5
-alpha <- 1.45
+scale_parameter <- 1
+alpha <- 1
 #rho <- 100000
-#rho <- .001
-rho <- 1
+rho <- 10
+#rho <- 1
 rho_inverse <- 1/rho
 CDF_table <- subset(CDF_table, V1 == alpha)
 CDF_table <- subset(CDF_table, V2 == rho_inverse )
@@ -115,16 +116,34 @@ stat_function(fun=fun.1) + labs( x = "Log Scaled Time", y ="Log Scaled Comp CDF"
 
 if(alpha == 1)
 {
+
+CDF_table[,3] <- CDF_table[,3]*(2*D)*exp(2*pi*rho*D)
+
+CDF_table[,5] <- 1 - CDF_table[,5] 
+CDF_table[,6] <- 1 - CDF_table[,6] 
+CDF_table[,7] <- 1 - CDF_table[,7] 
+
+
+
+CDF_table[,5] <- CDF_table[,5]/(2*pi*rho*D)
+CDF_table[,6] <- CDF_table[,6]/(2*pi*rho*D)
+CDF_table[,7] <- CDF_table[,7]/(2*pi*rho*D)
+
+
+
 CDF_table[,3] <- log10(CDF_table[,3] )
-CDF_table[,5] <- log10(1 - CDF_table[,5] )
-CDF_table[,6] <- log10(1 - CDF_table[,6] )
-CDF_table[,7] <- log10(1 - CDF_table[,7] )
+CDF_table[,5] <- log10(CDF_table[,5] )
+CDF_table[,6] <- log10(CDF_table[,6] )
+CDF_table[,7] <- log10(CDF_table[,7] )
 
 
-#fun.1 <- function(x)    log10(2*pi*rho*D) -log10(log(10)*x + log(2*D) + 2*pi*rho*D ) #log10(2*pi*rho*D) -log10(log(10)*x + log(10)*log(2*D) + 2*pi*rho*D )    #log(2*pi*rho*D/( 2*pi*rho*D+ log(1 +D*2*exp(x))))
+
+#fun.1 <- function(x)      log10(2*pi*rho*D) -log10(log(10)*x + log(2*D)  + 2*pi*rho*D ) #log10(2*pi*rho*D) -log10(log(10)*x + log(10)*log(2*D) + 2*pi*rho*D )    
+fun.1 <- function(x)      - log10(log(10)*x) 
+#log(2*pi*rho*D/( 2*pi*rho*D+ log(1 +D*2*exp(x))))
 p <- ggplot(CDF_table, aes(V3, V5)) +
-  geom_point() +  geom_pointrange(data=CDF_table, aes(x = V3, y =V5, ymin =V7, ymax =V6), pch  = 0) #+
-#stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0"))
+  geom_point() +  geom_pointrange(data=CDF_table, aes(x = V3, y =V5, ymin =V7, ymax =V6), pch  = 0) +
+stat_function(fun=fun.1) + labs( x = "Log Time", y ="Log Comp CDF") + ggtitle(paste("Alpha", alpha, "Distance 0"))
  
   
   print(p)
