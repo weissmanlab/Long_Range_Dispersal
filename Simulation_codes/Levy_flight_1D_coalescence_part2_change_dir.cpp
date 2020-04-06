d1057b309534c9f30b2ebd971ad0e11649980590
Tyler Smith <tylersmith@Tylers-MBP.attlocal.net>
2020-04-06 00:45:53 -0400

    Both parts of c++ codes are much cleaner and easier to read now.  Test the latest version thoroughly

--- Simulation_codes/Levy_flight_1D_coalescence_part2_change_dir.cpp
+++ Simulation_codes/Levy_flight_1D_coalescence_part2_change_dir.cpp
@@ -1,204 +1,86 @@
 #include "part2.h"
 int main(int argc, char* argv[])
-{         if(argc != 6) {cout << "Wrong number of arguments.  Arguments are alpha, initial distance, number of trials, total number of time steps, rho_inverse " << endl; return 0;} 
+{         if(argc != 7) {cout << "Wrong number of arguments.  Arguments are alpha, initial distance, number of trials, total number of time steps, scale_parameter, rho_inverse " << endl; return 0;} 
    
-   const double alpha = atof(argv[1]);  // controls power law tail of jump kernel
-  double initial_position = atof(argv[2]) ;  // initial signed distance between individuals
-const int num_trials = atoi(argv[3]);
-const int num_time_steps = atoi(argv[4]);
-  const double rho_inverse = atof(argv[5]); 
-  int seed = time(0);
-  std::mt19937 generator(seed);
-  std::uniform_real_distribution<double> uniform_dist(0.0, num_trials);
-  double mean_homozygosity[num_mu_steps] = {0}; //probability of two individuals (lineages) being identical given initial seperation and mu
-  double CDF_of_DCT[num_cdf_steps] = {0};
-double mean_homozygosity_INDIVIDUAL_TRIAL[num_mu_steps] = {0}; // conditional expectation E[exp(-2mut)|path] ] = E[Hom|path]   
-double CDF_of_DCT_INDIVIDUAL_TRIAL[num_cdf_steps] = {0};
-  double *Contribution_from_each_trial = new double[num_trials];double *Contribution_from_each_trialEXPONENT= new double[num_trials];
-  double *dist_of_coalescent_times = new double[num_time_steps];double **List_of_single_trial_homozygosities = new double*[num_mu_steps];
-double **List_of_single_trial_CDF = new double*[num_cdf_steps];
-for(int i =0; i < num_trials; i++){Contribution_from_each_trial[i] = 0; Contribution_from_each_trialEXPONENT[i] = 0;}
-for(int i =0; i < num_time_steps; i++){dist_of_coalescent_times[i] = 0;}
-for (int mu = 0; mu < num_mu_steps; mu++) {List_of_single_trial_homozygosities[mu] = new double[num_trials];}
-for (int mu = 0; mu < num_mu_steps; mu++) {for(int i =0; i < num_trials; i++){List_of_single_trial_homozygosities[mu][i] = 0; }}
-for (int T = 0; T < num_cdf_steps; T++) {List_of_single_trial_CDF[T] = new double[num_trials];}
-for (int T = 0; T < num_cdf_steps; T++) {for(int i =0; i < num_trials; i++){List_of_single_trial_CDF[T][i] = 0; }}
-
-  chdir( alpha, initial_position, num_trials, num_time_steps, rho_inverse); // change directory
+const double alpha = atof(argv[1]);  // controls power law tail of jump kernel
+const double initial_position = atof(argv[2]) ;  // initial signed distance between individuals
+const    int num_trials = atoi(argv[3]);
+const    int num_time_steps = atoi(argv[4]);
+const double  scale_parameter = atof(argv[5]); 
+const double rho_inverse = atof(argv[6]); 
+const double prob_of_coalescence_this_timestep = 1- exp(-delta_function_height*rho_inverse*timestep);
+double *dist_of_coalescent_times = return_1d_pointer(num_trials);
+double **List_of_single_trial_homozygosities = return_2d_pointer(num_mu_steps,  num_trials );
+double **List_of_single_trial_CDF = return_2d_pointer(num_cdf_steps, num_trials );
+  
+chdir( alpha, initial_position, num_trials, num_time_steps, rho_inverse); // change directory
 /*******************************/
 //Now that we're in the proper directory with the entrance and exit times of interest, 
 //we read them in and calculate the distribution of coalescence times and the mean homozygosity
 /**************************************/
 for(int trial =0; trial < num_trials; trial++)
- {  Contribution_from_each_trial[trial] = 0;
-     Contribution_from_each_trialEXPONENT[trial] = 0;   
-       for( int mu = 0; mu < num_mu_steps; mu++){ mean_homozygosity_INDIVIDUAL_TRIAL[mu] =  0;}
-     for( int T = 0; T < num_cdf_steps; T++){CDF_of_DCT_INDIVIDUAL_TRIAL[T] =  0;} 
-         
-          char INPUTFILE_entrance_and_exit_times[50];
-  sprintf(INPUTFILE_entrance_and_exit_times, "entrance_and_exit_times");
-  std::stringstream file_name_entrance_and_exit_times;
-         file_name_entrance_and_exit_times <<  INPUTFILE_entrance_and_exit_times  << 
-         "alpha" << alpha << "distance" << initial_position <<  "trial" << trial << ".txt" ;
-         std::string stringfile_entrance_and_exit_times;
-         file_name_entrance_and_exit_times >> stringfile_entrance_and_exit_times; 
+ {  Contribution_from_each_trial= 0;
+    Contribution_from_each_trialEXPONENT= 0;   
+    prob_of_no_coalescence_yet = 1; 
+          
+    // Here we're opening the file containing entrance times associated with each individual trial         
     ifstream fin_entrance_and_exit_times;
-    /****************************************/
-    // Here we're opening the file containing entrance times associated with each individual trial
-    fin_entrance_and_exit_times.open(stringfile_entrance_and_exit_times);
- double entrance_time = -1.0 ;  // If file is empty entrance and exit time will be the same and while loops will be ignored - dist of coalescent times will remain zero
-  double exit_time= -1.0 ;
+    input_filename = return_input_filename(alpha, scale_parameter, initial_position, num_trials, num_time_steps, trial  );
+    fin_entrance_and_exit_times.open(input_filename);
 
-////TIME LOOP BELOW
+    /****************************************/ 
+   
+    double entrance_time = -1.0 ;  // If file is empty entrance and exit time will be the same and while loops will be ignored - dist of coalescent times will remain zero
+    double exit_time= -1.0 ;
 
-for (int time =0; time < num_time_steps; time++) {
-if (timestep*double(time) >= entrance_time && timestep*double(time) < exit_time) //update exponent and add to dist_of_coalescent_times[time] when in coalescence zone
-{Contribution_from_each_trial[trial] =  exp(-Contribution_from_each_trialEXPONENT[trial])*(1- exp(-delta_function_height*rho_inverse*timestep));
- dist_of_coalescent_times[time] += Contribution_from_each_trial[trial]/num_trials;
- Contribution_from_each_trialEXPONENT[trial] += delta_function_height*rho_inverse*timestep;
- // for future times we account for all the time we spent sitting in the coalescence zone during this timestep
-for( int mu = 0; mu < num_mu_steps; mu++)
-     {mean_homozygosity_INDIVIDUAL_TRIAL[mu] +=  Contribution_from_each_trial[trial]*exp(-pow(10, mu)*2*mu_step*time*timestep); // extra factor of 2 in exponent of Laplace transform is standard in pop gen
-      List_of_single_trial_homozygosities[mu][trial] = mean_homozygosity_INDIVIDUAL_TRIAL[mu];   }
-for( int T = 0; T < num_cdf_steps; T++)
-  {if(exp(double(T)/2.0) > time ){
-    CDF_of_DCT_INDIVIDUAL_TRIAL[T] += Contribution_from_each_trial[trial]*timestep;
-     List_of_single_trial_CDF[T][trial] = CDF_of_DCT_INDIVIDUAL_TRIAL[T];}}
+    for (int time =0; time < num_time_steps; time++) 
+    {
+        time_value = double(time)*timestep;  // physical time, rather than integer increments of the timestep
+
+        if ( time_value >= entrance_time &&  time_value < exit_time) //update exponent and add to dist_of_coalescent_times[time] when in coalescence zone
+        { 
+         Contribution_from_each_trial = prob_of_no_coalescence_yet*prob_of_coalescence_this_timestep;
+         dist_of_coalescent_times[time] += Contribution_from_each_trial/num_trials;  // average over trials as we go  
+         prob_of_no_coalescence_yet *= exp(-delta_function_height*rho_inverse*timestep);  // update probability we havent coalesced
+   
+  
+         for( int mu = 0; mu < num_mu_steps; mu++)
+            { mu_value = pow(10, mu)*mu_step;
+              List_of_single_trial_homozygosities[mu][trial] +=  Contribution_from_each_trial*exp(-2.0*mu_value*time_value);
+
+            }
+  
+
+         for( int T = 0; T < num_cdf_steps; T++)
+            { T_value = exp(double(T)/2.0)*timestep;      
+              if( T_value > time_value ){ List_of_single_trial_CDF[T][trial] += Contribution_from_each_trial*timestep;}
+             
+            }
+
+         }
+
+           if (time_value >= exit_time){fin_entrance_and_exit_times >> entrance_time >> exit_time;} // read in new entrance and exit times
+
+       }
+
+     fin_entrance_and_exit_times.close();
+     // bin histogram of single trial homozygosities here.
+     Bin_single_trial_MH_and_CDF_into_histogram(num_trials, trial, List_of_single_trial_homozygosities, List_of_single_trial_CDF ); //, List_of_single_trial_homozygosities, List_of_single_trial_CDF);
 }
-if (timestep*double(time) >= exit_time){fin_entrance_and_exit_times >> entrance_time >> exit_time;}
-}
-/// TIME LOOP ABOVE
-  fin_entrance_and_exit_times.close();
- // bin histogram of single trial homozygosities here.
-         for (int mu =0; mu < num_mu_steps; mu++){for(int QQ = 0; QQ < num_histogram_bins; QQ++)
-             {if( mean_homozygosity_INDIVIDUAL_TRIAL[mu]  >= double(QQ)/double(num_histogram_bins) && mean_homozygosity_INDIVIDUAL_TRIAL[mu] < double(QQ + 1)/double(num_histogram_bins) )
-                {hist_of_single_trial_homozygosities[mu][QQ] += 1/double(num_trials);}
-              }}         
-           for (int T =0; T < num_cdf_steps; T++){for(int QQ = 0; QQ < num_histogram_bins; QQ++)
-             {if( CDF_of_DCT_INDIVIDUAL_TRIAL[T]  >= double(QQ)/double(num_histogram_bins) && CDF_of_DCT_INDIVIDUAL_TRIAL[T] < double(QQ + 1)/double(num_histogram_bins) )
-                { hist_of_single_trial_cdfs[T][QQ] += 1/double(num_trials);  }           
-              }}
 
-}
-//Laplace transform dist of coalescent times to get mean homozygosity
-for (int mu =0; mu < num_mu_steps; mu++){for (int time =0; time < num_time_steps; time++) {mean_homozygosity[mu] += dist_of_coalescent_times[time]*exp(-pow(10,mu)*2*mu_step*time*timestep); }}
-for (int time =0; time < num_time_steps; time++) { for(int T =0; T < num_cdf_steps; T++) {if(exp(double(T)/2.0) > time){CDF_of_DCT[T] += dist_of_coalescent_times[time]*timestep;}}}
-// The loops above calculates the mean homozygosity and CDF
-
-
-
+LAPLACE_TRANSFORM_DCT_TO_MH(dist_of_coalescent_times, num_time_steps); // get mean homozygosity from dct
+INTEGRATE_DCT_TO_CDF(dist_of_coalescent_times, num_time_steps); // get CDF from dct
 /*******************************************/
-// The loops below calculates the confidence interval of the mean homozygosity and CDF
-double dummy_rand1 = 0;
-int dummy_rand1_INDEX = 0;
-// generate list of sample means
-for( int mu = 0; mu < num_mu_steps; mu++)
-{for(int sample = 0; sample < num_samples_bootstrapped_means; sample++)
-   {  for(int trial =0; trial < num_trials; trial++)
-         {  dummy_rand1 = floor(uniform_dist(generator));    // we generate random numbers according to the single homozygosity histogram via inverse transform sampling
-            dummy_rand1_INDEX = int(dummy_rand1);  //This is the index associated to the above domain value for the Quantile function array
-            List_of_bootstrapped_mean_homozygosities[mu][sample] += List_of_single_trial_homozygosities[mu][dummy_rand1_INDEX]/double(num_trials);
-          }   
-         Sorted_List_of_bootstrapped_mean_homozygosities[mu][sample] = List_of_bootstrapped_mean_homozygosities[mu][sample];
-
-   }
-}
-// SORT BOOTSTRAPPED HOMOZYGOSITIES BUBBLE SORT 
-double tmp1 = 0;
-double tmp2 = 0;
-for( int mu = 0; mu < num_mu_steps; mu++)
-{ for(int sample1 = 0; sample1 < num_samples_bootstrapped_means; sample1++)
-    {for(int sample2 = sample1; sample2 < num_samples_bootstrapped_means; sample2++)
-    {    tmp1 = Sorted_List_of_bootstrapped_mean_homozygosities[mu][sample1]; tmp2 = Sorted_List_of_bootstrapped_mean_homozygosities[mu][sample2];
-         if( tmp2 < tmp1){  Sorted_List_of_bootstrapped_mean_homozygosities[mu][sample1] = tmp2; Sorted_List_of_bootstrapped_mean_homozygosities[mu][sample2] = tmp1;}
-    }
-    }
- }
-// USE SORTED LIST TO GET HISTOGRAM OF BOOSTRAPPED MEAN HOMOZYGOSITIES
-//
-for( int mu = 0; mu < num_mu_steps; mu++)
-{for(int sample = 0; sample < num_samples_bootstrapped_means; sample++)
-  { for(int QQ = 0; QQ < num_histogram_bins; QQ++)
-   {if( List_of_bootstrapped_mean_homozygosities[mu][sample]  >= double(QQ)/double(num_histogram_bins) && List_of_bootstrapped_mean_homozygosities[mu][sample] < double(QQ + 1)/double(num_histogram_bins) )
-                {hist_of_bootstrapped_mean_homozygosities[mu][QQ] += 1/double(num_samples_bootstrapped_means); }
-   }  
-  }
-}
-
-
-// USE HISTOGRAM TO GET CONDIFENCE INTERVALS
-int lower_CI_INDEX[num_mu_steps] = {0};
-int upper_CI_INDEX[num_mu_steps] = {0};
-for( int mu = 0; mu < num_mu_steps; mu++)
-{   lower_CI_INDEX[mu] = int(floor(double(num_samples_bootstrapped_means)*double(.16))); 
-    upper_CI_INDEX[mu] = int(floor(double(num_samples_bootstrapped_means)*double(.84)));
-}
-double lower_CI[num_mu_steps];
-double upper_CI[num_mu_steps];
-for( int mu = 0; mu < num_mu_steps; mu++)
-{lower_CI[mu]= Sorted_List_of_bootstrapped_mean_homozygosities[mu][lower_CI_INDEX[mu]];    // confidence interval for mean homozygosity obtained via bootstrapping
-upper_CI[mu]= Sorted_List_of_bootstrapped_mean_homozygosities[mu][upper_CI_INDEX[mu]];
-}
-
-// REPEAT THE ABOVE STEPS FOR THE CDF OF COALESCENCE TIMES
-dummy_rand1 = 0;
-dummy_rand1_INDEX = 0;
-// generate list of sample means
-for( int T = 0; T < num_cdf_steps; T++)
-{for(int sample = 0; sample < num_samples_bootstrapped_cdfs; sample++)
-   {  for(int trial =0; trial < num_trials; trial++)
-         {dummy_rand1 = floor(uniform_dist(generator));    // we generate random numbers according to the single homozygosity histogram via inverse transform sampling
-          dummy_rand1_INDEX = int(dummy_rand1);  //This is the index associated to the above domain value for the Quantile function array
-              List_of_bootstrapped_cdfs[T][sample] += List_of_single_trial_CDF[T][dummy_rand1_INDEX]/double(num_trials);
-           }       
-                Sorted_List_of_bootstrapped_cdfs[T][sample] = List_of_bootstrapped_cdfs[T][sample];
-   }
-
-}
-// now just sort the list of bootstrapped cdfs via bubble sort
-tmp1 = 0;
-tmp2 = 0;
-for( int T = 0; T < num_cdf_steps; T++)
-{ for(int sample1 = 0; sample1 < num_samples_bootstrapped_cdfs; sample1++)
-    { for(int sample2 = sample1; sample2 < num_samples_bootstrapped_cdfs; sample2++)
-      {  tmp1 = Sorted_List_of_bootstrapped_cdfs[T][sample1];
-         tmp2 = Sorted_List_of_bootstrapped_cdfs[T][sample2];
-        if( tmp2 < tmp1){  Sorted_List_of_bootstrapped_cdfs[T][sample1] = tmp2; Sorted_List_of_bootstrapped_cdfs[T][sample2] = tmp1; }
-      }
-    }
- }
-
-// use list of sample cdfs to get histogram of sample cdf values
-for( int T = 0; T < num_cdf_steps; T++)
-{for(int sample = 0; sample < num_samples_bootstrapped_cdfs; sample++)
-  {for(int QQ = 0; QQ < num_histogram_bins; QQ++)
-     {  if( List_of_bootstrapped_cdfs[T][sample]  >= double(QQ)/double(num_histogram_bins) && List_of_bootstrapped_cdfs[T][sample] < double(QQ + 1)/double(num_histogram_bins) )
-                {  hist_of_bootstrapped_cdfs[T][QQ] += 1/double(num_samples_bootstrapped_means);}
-     } 
-  }
-}
-
-int lower_CI_INDEX_CDF[num_cdf_steps] = {0};
-int upper_CI_INDEX_CDF[num_cdf_steps] = {0};
-for( int T = 0; T < num_cdf_steps; T++)
-{   lower_CI_INDEX_CDF[T] = int(floor(double(num_samples_bootstrapped_cdfs)*double(.16)));  
-    upper_CI_INDEX_CDF[T] = int(floor(double(num_samples_bootstrapped_cdfs)*double(.84)));
-}
-double lower_CI_CDF[num_cdf_steps];
-double upper_CI_CDF[num_cdf_steps];
-for( int T = 0; T < num_cdf_steps; T++)
-{lower_CI_CDF[T]= Sorted_List_of_bootstrapped_cdfs[T][lower_CI_INDEX_CDF[T]];    // confidence interval for mean homozygosity obtained via bootstrapping
-upper_CI_CDF[T]= Sorted_List_of_bootstrapped_cdfs[T][upper_CI_INDEX_CDF[T]];
-}
-/*******************************************/
+// Calculate the confidence intervals using bootstrap
+BOOTSTRAP_CI_MH_and_CDF(alpha, initial_position, num_trials, num_time_steps, rho_inverse, List_of_single_trial_homozygosities, List_of_single_trial_CDF);
 //Now we output our results to files
-/*******************************************/
-print_Mean_Homozygosity(alpha, initial_position, num_trials, num_time_steps, rho_inverse, mean_homozygosity, lower_CI, upper_CI);
-print_CDF_of_coalescence_times(alpha, initial_position, num_trials, num_time_steps, rho_inverse, CDF_of_DCT,  lower_CI_CDF, upper_CI_CDF);
-print_histogram_of_single_trial_homozygosities(alpha, initial_position, num_trials, num_time_steps, rho_inverse, hist_of_single_trial_homozygosities);
-print_histogram_of_bootstrapped_mean_homozygosities(alpha, initial_position, num_trials, num_time_steps, rho_inverse, hist_of_bootstrapped_mean_homozygosities);
-//print_Sorted_List_of_bootstrapped_mean_homozygosities(alpha, initial_position, num_trials, num_time_steps, rho_inverse, Sorted_List_of_bootstrapped_mean_homozygosities);
-//print_list_of_single_trial_homozygosities(alpha, initial_position, num_trials, num_time_steps, rho_inverse, List_of_single_trial_homozygosities);
+print_Mean_Homozygosity(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, mean_homozygosity, lower_CI, upper_CI);
+print_CDF_of_coalescence_times(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, CDF_of_DCT,  lower_CI_CDF, upper_CI_CDF);
+print_histogram_of_single_trial_homozygosities(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, hist_of_single_trial_homozygosities);
+print_histogram_of_bootstrapped_mean_homozygosities(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, hist_of_bootstrapped_mean_homozygosities);
+print_Sorted_List_of_bootstrapped_mean_homozygosities(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, Sorted_List_of_bootstrapped_mean_homozygosities);
+print_list_of_single_trial_homozygosities(alpha, scale_parameter, initial_position, num_trials, num_time_steps, rho_inverse, List_of_single_trial_homozygosities);
+
 chdir("..");
 chdir("..");  // return to original directory
 return 0;
--- Simulation_codes/part1.h
+++ Simulation_codes/part1.h
@@ -16,62 +16,68 @@
 #include <cfloat>
 using namespace std;
 
+double periodic_boundary = DBL_MAX;  //massive value, range is effectively infinite.
+double timestep = 1.0;
+double delta_function_width = 1.0; //width of coalescence zone 
+double signed_step_size; // used to increment random walk below
 
- 
+double return_rescaled_SD(double alpha, double scale_parameter)
+{  double ORIG_SD  = 2*sqrt(alpha*(4*alpha -2)/((2*alpha -2)*(2*alpha -2)*(2*alpha -4)));
+   return   (scale_parameter/ORIG_SD);
+//ORIG_STD is the standard deviation of the original F-distribution. 
 
- const double periodic_boundary = DBL_MAX;//100000000000; //position constrained between -pb and +pb 
-  const double levy_param = 1.00;
-  const double timestep = 1.0;//const double timestep = .1; // for deterministic drift term
-  const double delta_function_width = 1.0; //atof(argv[5]);;
-  // the scale parameter c is related to the generalized diffusion constant D as c =(4D*timestep)^(1/alpha)
-  double jump_size_levy = 0;
-  double jump_size_fisher = 0;
-
-//ORIG_STD is the standard deviation of the fisher distribution.  We rescale so that scale_parameter is the standard deviation.
-
-
+// We rescale so that scale_parameter is the standard deviation of the distribution we draw from. 
+// This is needed for alpha > 2 only; in this regime dispersal is not a true Levy flight.  
+// We probe these broader power laws with an F-distribution.
+}
 
 
 inline void chdir(double alpha, double initial_position, int num_trials, int num_time_steps, double scale_parameter)
 
 { std::stringstream file_name2;
-         file_name2 <<  "alpha_value_"  <<  alpha  ;   // This is the directory name
-         std::string stringfile2;
-         file_name2 >> stringfile2;         
-          std::stringstream file_name3;
-         file_name3 <<  "mkdir alpha_value_"  << alpha ;   // This is mkdir directory name
-         std::string stringfile3;
-         getline(file_name3, stringfile3); 
-char OUTPUTFILE_Create_Parent_Directory[50];
-strcpy(OUTPUTFILE_Create_Parent_Directory, stringfile3.c_str());
-system(OUTPUTFILE_Create_Parent_Directory);
-char OUTPUTFILE_Parent_Directory[50];
-strcpy(OUTPUTFILE_Parent_Directory, stringfile2.c_str());
-chdir(OUTPUTFILE_Parent_Directory);
+  file_name2 <<  "alpha_value_"  <<  alpha  ;   // This is the directory name
+  std::string stringfile2;
+  file_name2 >> stringfile2;         
+  std::stringstream file_name3;
+  file_name3 <<  "mkdir alpha_value_"  << alpha ;   // This is mkdir directory name
+  std::string stringfile3;
+  getline(file_name3, stringfile3); 
+  char OUTPUTFILE_Create_Parent_Directory[50];
+  strcpy(OUTPUTFILE_Create_Parent_Directory, stringfile3.c_str());
+  system(OUTPUTFILE_Create_Parent_Directory);
+  char OUTPUTFILE_Parent_Directory[50];
+  strcpy(OUTPUTFILE_Parent_Directory, stringfile2.c_str());
+  chdir(OUTPUTFILE_Parent_Directory);
 /********************************************/
    
-      std::stringstream file_name4;
-         file_name4 <<  "distance_value_"  <<  initial_position  ;   // This is the directory name
-         std::string stringfile4;
-         file_name4 >> stringfile4;   
-          std::stringstream file_name5;
-         file_name5 <<  "mkdir distance_value_"  << initial_position ;   // This is mkdir directory name
-         std::string stringfile5;
-         getline(file_name5, stringfile5); 
-char OUTPUTFILE_Create_Child_Directory[50];
-strcpy(OUTPUTFILE_Create_Child_Directory, stringfile5.c_str());
-system(OUTPUTFILE_Create_Child_Directory);
-char OUTPUTFILE_Child_Directory[50];
-strcpy(OUTPUTFILE_Child_Directory, stringfile4.c_str());
-chdir(OUTPUTFILE_Child_Directory);
+  std::stringstream file_name4;
+  file_name4 <<  "distance_value_"  <<  initial_position  ;   // This is the directory name
+  std::string stringfile4;
+  file_name4 >> stringfile4;   
+  std::stringstream file_name5;
+  file_name5 <<  "mkdir distance_value_"  << initial_position ;   // This is mkdir directory name
+  std::string stringfile5;
+  getline(file_name5, stringfile5); 
+  char OUTPUTFILE_Create_Child_Directory[50];
+  strcpy(OUTPUTFILE_Create_Child_Directory, stringfile5.c_str());
+  system(OUTPUTFILE_Create_Child_Directory);
+  char OUTPUTFILE_Child_Directory[50];
+  strcpy(OUTPUTFILE_Child_Directory, stringfile4.c_str());
+  chdir(OUTPUTFILE_Child_Directory);
 
 }
 
 
 
-
-
-
+inline string return_output_filename(double alpha, double scale_parameter, double initial_position, int trial )
+{ char INPUTFILE_entrance_and_exit_times[50];
+  sprintf(INPUTFILE_entrance_and_exit_times, "entrance_and_exit_times");
+  std::stringstream file_name_entrance_and_exit_times;
+  file_name_entrance_and_exit_times <<  INPUTFILE_entrance_and_exit_times << "_scale_parameter_" << scale_parameter << "_alpha" << alpha << "distance" << initial_position <<  "trial" << trial << ".txt" ;
+  std::string stringfile_entrance_and_exit_times;
+  file_name_entrance_and_exit_times >> stringfile_entrance_and_exit_times; 
+  return stringfile_entrance_and_exit_times;
+}
 
 
 
